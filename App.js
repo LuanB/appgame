3602e7a6d9ecccfe72daa35a19fe27c51a517383
Luan Bui <luanb3455@gmail.com>
2020-09-19 23:34:20 +0800

    cell logic

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import produce from 'immer';
 import {
   SafeAreaView,
@@ -32,6 +32,18 @@ import {FlatGrid} from 'react-native-super-grid';
 const numRows = 10;
 const numCols = 10;
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+  ,
+];
+
 const App = () => {
   const [grid, setGrid] = useState(() => {
     const rows = [];
@@ -43,10 +55,53 @@ const App = () => {
 
   const [running, setRunning] = useState(false);
 
+  const runningRef = useRef(running);
+
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    // simulate
+
+    setTimeout(runSimulation, 1000);
+  }, []);
+
   return (
     <>
       <Button
-        onPress={() => setRunning(!running)}
+        onPress={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
         title={running ? 'Stop' : 'Start'}
         color="#841584"
         accessibilityLabel="Start game"
@@ -82,6 +137,9 @@ const App = () => {
           ));
         }}
       />
+      <View>
+        <Text>Simulator is {running ? 'running' : 'has stopped'}</Text>
+      </View>
 
       {/* </View> {grid.map((rows, i) => */}
       {/* //   rows.map((col, colIndx) => (

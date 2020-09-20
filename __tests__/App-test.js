67804b74674333138cfaab739b8bf16a72b2b2ea
Luan Bui <luanb3455@gmail.com>
2020-09-20 14:22:18 +0800

    react native testing library snapshot testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,7 +8,15 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render} from 'react-native-testing-library';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const result = render(<App />).toJSON();
+    expect(result).toMatchSnapshot();
+  });
 });

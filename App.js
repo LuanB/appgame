dfacfb84463803dbc5f7e2b8ff419ab8aaccef03
Luan Bui <luanb3455@gmail.com>
2020-09-20 15:53:57 +0800

    tab navigators

--- App.js
+++ App.js
@@ -17,21 +17,74 @@ const App = () => {
   import React from 'react';
 
   import {
-  NavigationContainer,
+    NavigationContainer,
+      DefaultTheme,
+  DarkTheme,
 
 } from '@react-navigation/native';
   import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+  import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+  import { Appearance, useColorScheme, AppearanceProvider } from 'react-native-appearance';
+  
 
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-  DrawerActions
-} from '@react-navigation/native';
+  const MaterialBottomTabs = createMaterialBottomTabNavigator();
+
+
 
   App = () => { 
+  const colorScheme = useColorScheme();
+
+  const MyTheme = {
+    dark: false,
+    colors: {
+      primary: 'white',
+      background: 'white',
+      card: '#65509f',
+      text: 'white',
+      border: 'green',
+    },
+  }
 
 
-    
+createBottomTabs = () => {
+    return <MaterialBottomTabs.Navigator>
+      <MaterialBottomTabs.Screen
+        name="Tab 1"
+        style={{ marginBottom: 16 }}
+        component={Tab1}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: () => (
+            <Icon style={[{ color: 'white' }]} size={25} name={'home'} />
+          ),
+        }}
+      />
+      <MaterialBottomTabs.Screen name="Tab 2" component={Tab2}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => (
+            <Icon style={[{ color: 'white' }]} size={25} name={'human'} />
+          )
+        }}
+      />
+      <MaterialBottomTabs.Screen name="Tab 3" component={Tab3}
+        options={{
+          tabBarLabel: 'Map',
+          tabBarIcon: () => (
+            <Icon style={[{ color: 'white' }]} size={25} name={'map'} />
+          ),
+        }}
+      />
+    </MaterialBottomTabs.Navigator>
+  
+    return (
+    <AppearanceProvider>
+      <NavigationContainer theme={colorScheme == 'dark' ? DarkTheme : MyTheme}>
+        {this.createHomeStack()}
+      </NavigationContainer>
+    </AppearanceProvider>
+  );
+
+
   }
 export default App;
